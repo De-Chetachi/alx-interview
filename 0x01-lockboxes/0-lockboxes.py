7ebcc33ca7083c0335b9b5190baf58ddf02ccb6c
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-15 22:40:18 +0100

    optimizing...

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,22 +6,23 @@ if the keys to all the boxes are available in the boxes'''
 
 def canUnlockAll(boxes, open_boxes=set(), ava_keys=None):
     '''this function tries to find out if
-    all the boxes in list can be opened simultaneously
+    all the boxes in a list can be opened simultaneously
     parameter boxes: a list of lists, each child list represents a box
     each box contains keys
     return True if all the boxes can be opened else false'''
 
+    if not boxes:
+        return False
     if not ava_keys:
         ava_keys = set(boxes[0])
-    new_keys = []
+    new_keys = set()
     for key in ava_keys:
         try:
-            new_keys.extend(boxes[key])
+            new_keys.update(boxes[key])
         except Exception as e:
             continue
     open_boxes = open_boxes | ava_keys
-    keys_ = set(new_keys)
-    ava_keys = keys_ - open_boxes
+    ava_keys = new_keys - open_boxes
     if len(ava_keys) > 0:
         return canUnlockAll(boxes, open_boxes, ava_keys)
     else:
