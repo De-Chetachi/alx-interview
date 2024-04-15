898aabad20511287110f55ae316f913ca7b2ed74
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-15 22:04:07 +0100

    not optimal

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''this module contains a function canUnlockAll
+the function takes in a list of locked boxex and finds out
+if the keys to all the boxes are available in the boxes'''
+
+
+def canUnlockAll(boxes, open_boxes=set(), ava_keys=None):
+    '''this function tries to find out if
+    all the boxes in list can be opened simultaneously
+    parameter boxes: a list of lists, each child list represents a box
+    each box contains keys
+    return True if all the boxes can be opened else false'''
+
+    if not ava_keys:
+        ava_keys = set(boxes[0])
+    new_keys = []
+    for key in ava_keys:
+        try:
+            new_keys.extend(boxes[key])
+        except Exception as e:
+            continue
+    open_boxes = open_boxes | ava_keys
+    keys_ = set(new_keys)
+    ava_keys = keys_ - open_boxes
+    if len(ava_keys) > 0:
+        return canUnlockAll(boxes, open_boxes, ava_keys)
+    else:
+        for i in range(1, len(boxes)):
+            if i not in open_boxes:
+                return False
+        return True
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,2 @@
+this project is a datanstructure/ algorithm task 
+the aim is to find out if thhe keys of all boxex are available in a list of boxes
