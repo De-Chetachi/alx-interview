f23e81c4ac7467b87a72bd0b43254673babf0ca6
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-11-17 12:07:21 +0100

    island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,16 +6,19 @@ def island_perimeter(grid):
     '''a function that calculates the parimeter of a island
     args: grid: a 2d array'''
     rows = len(grid)
-    parimeter = 0;
+    parimeter = 0
     for i in range(rows):
         cols = len(grid[i])
         for j in range(cols):
             if grid[i][j]:
                 parimeter += 4
                 try:
-                    if grid[i][j + 1]:
-                        parimeter -= 2
                     if grid[i + 1][j]:
+                        parimeter -= 2
+                except IndexError:
+                    pass
+                try:
+                    if grid[i][j + 1]:
                         parimeter -= 2
                 except IndexError:
                     pass
---
+++ 0x09-island_perimeter/1-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+island_perimeter = __import__('0-island_perimeter').island_perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+    ]
+    print(island_perimeter(grid))
